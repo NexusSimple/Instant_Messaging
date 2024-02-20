acefab58f8f8fceae0c0de575dd05b2814d87006
Nexus Simple <nexus.simple7@gmail.com>
2024-02-20 22:12:37 +0530

    [FEATURE] Add some static and dynamic classes to the input element by using the "clsx" utility.

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import clsx from "clsx";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
@@ -42,6 +43,28 @@ const Input = ({
           autoComplete={id}
           disabled={disabled}
           {...register(id, { required })}
+          className={clsx(
+            `
+            form-input
+            block 
+            w-full 
+            rounded-md 
+            border-0 
+            py-1.5 
+            text-gray-900 
+            shadow-sm 
+            ring-1 
+            ring-inset 
+            ring-gray-300 
+            placeholder:text-gray-400 
+            focus:ring-2 
+            focus:ring-inset 
+            focus:ring-sky-600 
+            sm:text-sm 
+            sm:leading-6`,
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
         />
       </div>
     </div>

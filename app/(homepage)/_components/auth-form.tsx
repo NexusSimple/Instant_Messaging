2a150b7cce054e893ef81b8a5faba1f0a290584e
Nexus Simple <nexus.simple7@gmail.com>
2024-02-20 22:10:43 +0530

    [FEATURE] Also attach the id="email" prop to the rendered Input element in the AuthForm component.

--- app/(homepage)/_components/auth-form.tsx
+++ app/(homepage)/_components/auth-form.tsx
@@ -61,7 +61,7 @@ const AuthForm = () => {
         "
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input label="Email" register={register} />
+          <Input id="email" label="Email" register={register} />
         </form>
       </div>
     </div>

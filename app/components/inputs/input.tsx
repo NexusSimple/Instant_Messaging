99dcc17859266b9109a53111e57531048372c2f7
Nexus Simple <nexus.simple7@gmail.com>
2024-02-20 21:06:39 +0530

    [FEATURE] Create the interface for the props of the Input component and extract the props in the parameters.

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -1,6 +1,26 @@
 "use client";
 
-const Input = () => {
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+const Input = ({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}: InputProps) => {
   return <div>Input</div>;
 };
 

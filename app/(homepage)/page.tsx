7e049baa1370672535101f64a1dab16d2d76588b
Nexus Simple <nexus.simple7@gmail.com>
2024-02-20 17:37:53 +0530

    [FEATURE] Give some styles to the outermost div of the Home component i.e. the root page.tsx file.

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>Instant Messaging App</div>;
+  return (
+    <div
+      className="
+        flex 
+        min-h-full 
+        flex-col 
+        justify-center 
+        py-12 
+        sm:px-6 
+        lg:px-8 
+        bg-gray-100
+      "
+    >
+      Instant Messaging App
+    </div>
+  );
 }

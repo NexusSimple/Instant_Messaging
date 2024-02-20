33d04ab67ca83dab1aa9336750f3bcb213b63357
Nexus Simple <nexus.simple7@gmail.com>
2024-02-20 17:58:13 +0530

    [FEATURE] Add primary & secondary color in the "tailwind.config.ts" file matching the logo.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#31ccf5",
+        secondary: "#414afa",
+      },
+    },
   },
   plugins: [],
 };

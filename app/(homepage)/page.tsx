7078f4a21c62920d7a70254349e4b69d0700a408
Nexus Simple <nexus.simple7@gmail.com>
2024-02-20 18:43:43 +0530

    [FEATURE] Decrease the size of the Logo file in the svg .
    - We need to remove the px in the height and width.

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
     >
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
-          height="16"
-          width="16"
+          height="48"
+          width="48"
           className="mx-auto w-auto"
           src="/images/logo.svg"
           alt="Logo"

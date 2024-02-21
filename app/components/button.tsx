f692f79126d2a85963fdb2fbeb78c66634e9acf2
Nexus Simple <nexus.simple7@gmail.com>
2024-02-21 21:13:35 +0530

    [FEATURE] Create the interface for the props of the Button component and extract the props in the paramters of the component.

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,6 +1,24 @@
-"use client"
+"use client";
 
-const Button = () => {
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
+const Button = ({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}: ButtonProps) => {
   return <div>Button</div>;
 };
 

54761b61aaa2b65620c7248d86b084b8a475e90b
Nexus Simple <nexus.simple7@gmail.com>
2024-02-23 11:22:37 +0530

    [FEATURE] Import the necessary files for NextAuth configuration.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,6 @@
 import bcrypt from "bcrypt";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";

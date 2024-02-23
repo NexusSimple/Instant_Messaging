6f5166336a7cb0f400de56cb1aac020148ba37f9
Nexus Simple <nexus.simple7@gmail.com>
2024-02-23 22:00:20 +0530

    [FEATURE] Remove the button from the Users page and add a div with some styles.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import { signOut } from "next-auth/react";
 
 const Users = () => {
-  return <button onClick={() => signOut()}>Log Out</button>;
+  return <div className="hidden lg:block lg:pl-80 h-full"></div>;
 };
 
 export default Users;

2501c555c0a9a4b60d9af3972aed8fbe37fc9b03
Nexus Simple <nexus.simple7@gmail.com>
2024-02-23 11:42:57 +0530

    [FEATURE] Extract the body and its contents from the request to this API endpoint i.e. /api/register

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,3 +1,7 @@
 import bcrypt from "bcrypt";
 
-export async function POST(req: Request) {}
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { name, email, password } = body;
+}

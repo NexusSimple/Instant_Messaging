eed1c161365d371e6ac11935e7bff0d7ad48b827
Nexus Simple <nexus.simple7@gmail.com>
2024-02-23 22:09:15 +0530

    [FEATURE] Complete the remaining template of the EmptyState component.

--- app/components/empty-state.tsx
+++ app/components/empty-state.tsx
@@ -14,7 +14,11 @@ const EmptyState = () => {
         bg-gray-100
     "
     >
-      Empty State component
+      <div className="text-center items-center flex flex-col">
+        <h3 className="mt-2 text-2xl font-semibold text-gray-900">
+          Select a chat or start a new conversation
+        </h3>
+      </div>
     </div>
   );
 };

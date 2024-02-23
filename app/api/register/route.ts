6010f7dd9fdfd23adf1885d34a7d4a465e1619e2
Nexus Simple <nexus.simple7@gmail.com>
2024-02-23 11:46:16 +0530

    [FEATURE] Create the rest of the functionality of the API route handler for Registering a user.

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,7 +1,25 @@
+import { prismadb } from "@/app/libs/prismadb";
 import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const body = await req.json();
 
   const { name, email, password } = body;
+
+  if (!name || !email || !password) {
+    return new NextResponse("Missing info", { status: 400 });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prismadb.user.create({
+    data: {
+      name: name,
+      email: email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
 }

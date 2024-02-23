f6cc0c34be51eebbd237a24f97b176240aced1da
Nexus Simple <nexus.simple7@gmail.com>
2024-02-23 13:57:30 +0530

    [FEATURE] Display a toast notification in the Client Side when there is an error in the POST request to "/api/register" by incorporating a catch block.

--- app/(homepage)/_components/auth-form.tsx
+++ app/(homepage)/_components/auth-form.tsx
@@ -6,6 +6,7 @@ import Input from "@/app/components/inputs/input";
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 
@@ -39,7 +40,9 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      axios.post("/api/register", data);
+      axios.post("/api/register", data).catch(() => {
+        toast.error("Something went wrong");
+      });
     }
 
     if (variant === "LOGIN") {

657cf28db3848ec361d5eb966eea6b52d262f223
Nexus Simple <nexus.simple7@gmail.com>
2024-02-23 22:07:12 +0530

    [FEATURE] Add some styles to the outermost div of the EmptyState component.

--- app/components/empty-state.tsx
+++ app/components/empty-state.tsx
@@ -1,5 +1,22 @@
 const EmptyState = () => {
-  return <div>Empty State component</div>;
+  return (
+    <div
+      className="
+        px-4 
+        py-10 
+        sm:px-6 
+        lg:px-8 
+        lg:py-6 
+        h-full 
+        flex 
+        justify-center 
+        items-center 
+        bg-gray-100
+    "
+    >
+      Empty State component
+    </div>
+  );
 };
 
 export default EmptyState;

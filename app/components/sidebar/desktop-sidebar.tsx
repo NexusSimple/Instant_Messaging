1cb93cc587cd94733ea6c561711aedee809b24b5
Nexus Simple <nexus.simple7@gmail.com>
2024-02-24 11:48:38 +0530

    [FEATURE] Create a Boolean useState "isOpen" and default it to false.

--- app/components/sidebar/desktop-sidebar.tsx
+++ app/components/sidebar/desktop-sidebar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
 
 const DesktopSidebar = () => {
   const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
   return <div>DesktopSidebar</div>;
 };
 

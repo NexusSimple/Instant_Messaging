6c777701e9760ea36f03ec06fede61bede05a69a
Nexus Simple <nexus.simple7@gmail.com>
2024-02-24 19:09:13 +0530

    [FEATURE] Complete the template of the MobileItem component with a Link element and an Icon .

--- app/components/sidebar/mobile-item.tsx
+++ app/components/sidebar/mobile-item.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import Link from "next/link";
+
 interface MobileItemProps {
   href: string;
   icon: any;
@@ -11,7 +14,31 @@ const MobileItem = ({ href, icon: Icon, active, onClick }: MobileItemProps) => {
       return onClick();
     }
   };
-  return <div>MobileItem</div>;
+  return (
+    <Link
+      onClick={handleClick}
+      href={href}
+      className={clsx(
+        `
+    group 
+    flex 
+    gap-x-3 
+    text-sm 
+    leading-6 
+    font-semibold 
+    w-full 
+    justify-center 
+    p-4 
+    text-gray-500 
+    hover:text-black 
+    hover:bg-gray-100
+  `,
+        active && "bg-gray-100 text-black"
+      )}
+    >
+      <Icon className="h-6 w-6" />
+    </Link>
+  );
 };
 
 export default MobileItem;

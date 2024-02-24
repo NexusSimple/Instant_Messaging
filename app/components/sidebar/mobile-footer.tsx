11e82625fe8b86932efb207ef1018ad6ce67b1a7
Nexus Simple <nexus.simple7@gmail.com>
2024-02-24 15:56:18 +0530

    [FEATURE] Extract the routes and the isOpen variable from the respective custom hooks created earlier.

--- app/components/sidebar/mobile-footer.tsx
+++ app/components/sidebar/mobile-footer.tsx
@@ -1,4 +1,9 @@
+import useConversation from "@/app/hooks/useConversation";
+import useRoutes from "@/app/hooks/useRoutes";
+
 const MobileFooter = () => {
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
   return <div>Mobile Footer</div>;
 };
 

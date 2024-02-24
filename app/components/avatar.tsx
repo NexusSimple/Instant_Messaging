bfa6888bce8a01c0334426fa8448f8b39cf64283
Nexus Simple <nexus.simple7@gmail.com>
2024-02-24 20:38:46 +0530

    [FEATURE] Create the interface for the prop "user" of the Avatar component and extract the prop in the parameters.

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -1,6 +1,12 @@
-"use client"
+"use client";
 
-const Avatar = () => {
+import { User } from "@prisma/client";
+
+interface AvatarProps {
+  user?: User;
+}
+
+const Avatar = ({ user }: AvatarProps) => {
   return <div>Avatar</div>;
 };
 

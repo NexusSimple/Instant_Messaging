d57bff440ec861b633934da29247d307cac6d44c
Nexus Simple <nexus.simple7@gmail.com>
2024-02-25 11:30:26 +0530

    [FEATURE] Create the handleClick for handling the functionality when the current user clicks on a user out of the list of the users which are displayed to the current user.

--- app/users/components/user-box.tsx
+++ app/users/components/user-box.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { User } from "@prisma/client";
+import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 interface UserBoxProps {
   data: User;
@@ -11,6 +12,22 @@ interface UserBoxProps {
 const UserBox = ({ data }: UserBoxProps) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setIsLoading(true);
+
+    // Start a conversation by passing the user id of the user that the current user wants to start the conversation with.
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then(() => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [data, router]);
   return <div>User</div>;
 };
 

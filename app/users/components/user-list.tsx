ea4c489c4d426d19e0de482d729e0f983aea777f
Nexus Simple <nexus.simple7@gmail.com>
2024-02-25 10:47:07 +0530

    [FEATURE] As the template of the UserList component, render an <aside> element as the outermost element of the template with some styles to it.

--- app/users/components/user-list.tsx
+++ app/users/components/user-list.tsx
@@ -6,7 +6,23 @@ interface UserListProps {
   items: User[];
 }
 const UserList = ({ items }: UserListProps) => {
-  return <div>User List component</div>;
+  return (
+    <aside
+      className="
+    fixed 
+    inset-y-0 
+    pb-20
+    lg:pb-0
+    lg:left-20 
+    lg:w-80 
+    lg:block
+    overflow-y-auto 
+    border-r 
+    border-gray-200
+    block w-full left-0
+  "
+    ></aside>
+  );
 };
 
 export default UserList;

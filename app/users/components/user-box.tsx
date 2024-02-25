d84e8470cbbc53968eb478d949a2d0a83a41912b
Nexus Simple <nexus.simple7@gmail.com>
2024-02-25 11:18:15 +0530

    [FEATURE] Instantiate a new instance of useRouter() hook and also create a useState "isLoading" and default it to false.

--- app/users/components/user-box.tsx
+++ app/users/components/user-box.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface UserBoxProps {
   data: User;
 }
 
 const UserBox = ({ data }: UserBoxProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   return <div>User</div>;
 };
 

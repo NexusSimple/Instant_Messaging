244b3e07648aec52be26db86fcd09c1c9383eb71
Nexus Simple <nexus.simple7@gmail.com>
2024-02-26 19:05:28 +0530

    [FEATURE] Protect the /conversations API endpoint and subsequent routes from it.

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export default withAuth({
 
 // Config to protect routes included in the matcher.
 export const config = {
-  matcher: ["/users/:path*"], // Including "/:path*" means "/users/anything/anything2/anything3" etc
+  matcher: ["/users/:path*", "/conversations/:path*"], // Including "/:path*" means "/users/anything/anything2/anything3" etc
 };
 
 //  The ":path*" part is a dynamic route parameter that matches any subsequent paths after "/users/".

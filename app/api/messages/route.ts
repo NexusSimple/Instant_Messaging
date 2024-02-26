8e59b15ab088cd9aefeda5aef395325abc64d598
Nexus Simple <nexus.simple7@gmail.com>
2024-02-26 13:16:48 +0530

    [FEATURE] Change the Error Message in the NextResponse( ) method.

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -4,6 +4,6 @@ export async function POST(request: Request) {
   try {
   } catch (error: any) {
     console.log("[MESSAGES]", error);
-    return new NextResponse("Error", { status: 500 });
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 }

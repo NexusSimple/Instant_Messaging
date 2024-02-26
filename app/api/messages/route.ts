cfa72e9d85e0b150c7d9efc718546b41654368d9
Nexus Simple <nexus.simple7@gmail.com>
2024-02-26 13:25:33 +0530

    [FEATURE] Add the try block logic of creating a new message in the DB and also updating the conversation table in the DB.

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,7 +1,62 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { prismadb } from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
+    const currentUser = await getCurrentUser();
+    const body = await request.json();
+    const { message, image, conversationId } = body;
+
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const newMessage = await prismadb.message.create({
+      include: {
+        seen: true,
+        sender: true,
+      },
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: { id: conversationId },
+        },
+        sender: {
+          connect: { id: currentUser.id },
+        },
+        seen: {
+          connect: {
+            id: currentUser.id,
+          },
+        }, // Since the current user who has sent the message, so automatically push the current use to the array of "seen" users.
+      },
+    });
+
+    const updatedConversation = await prismadb.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage);
   } catch (error: any) {
     console.log("[MESSAGES]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
